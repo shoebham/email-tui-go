85c4a2f51b33922cd28e2285e258ff8e711cbdb2
Shubham <25881429+shoebham@users.noreply.github.com>
2025-06-02 20:36:19 +0530

    add email view

--- imap.go
+++ imap.go
@@ -1,1 +1,71 @@
 package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+	username := os.Getenv("IMAP_USERNAME")
+	password := os.Getenv("IMAP_PASSWORD")
+	if username == "" || password == "" {
+		log.Fatal("IMAP credentials not set in environment variables")
+	}
+
+	// Connect to server
+	c, err := client.DialTLS("imap.gmail.com:993", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Logout()
+
+	// Login
+	if err := c.Login(username, password); err != nil {
+		log.Fatal(err)
+	}
+
+	// Select INBOX
+	mbox, err := c.Select("INBOX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if mbox.Messages == 0 {
+		log.Println("No messages")
+		return
+	}
+
+	// Fetch the last 10 messages
+	from := uint32(1)
+	if mbox.Messages > 10 {
+		from = mbox.Messages - 9
+	}
+	to := mbox.Messages
+
+	seqSet := new(imap.SeqSet)
+	seqSet.AddRange(from, to)
+
+	messages := make(chan *imap.Message, 10)
+	go func() {
+		err = c.Fetch(seqSet, []imap.FetchItem{imap.FetchEnvelope}, messages)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//close(messages)
+	}()
+
+	for msg := range messages {
+		fmt.Printf("[Message Time: %s] Message Subject: %s\n",
+			msg.Envelope.Date.Format("2006-01-02 15:04:05"),
+			msg.Envelope.Subject)
+	}
+}
--- inbox.go
+++ inbox.go
@@ -9,7 +9,19 @@ import (
 )
 
 type inboxModel struct {
-	mails list.Model
+	mails    list.Model
+	selected list.Item
+}
+
+type emailModel struct {
+	subject  string
+	body     string
+	sender   string
+	receiver string
+	date     string
+}
+type emailMsg struct {
+	email emailModel
 }
 
 type inboxMsg struct {
@@ -92,6 +104,23 @@ func (m inboxModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
+		case "enter":
+			m.selected = m.mails.SelectedItem()
+			selectedEmail := m.mails.SelectedItem().(item)
+			emailDetails := emailModel{
+				subject:  selectedEmail.title,
+				body:     selectedEmail.description,
+				sender:   "x",
+				receiver: "y",
+			}
+			return updateEmailView(msg, m, emailDetails)
+		case "backspace":
+			if m.selected != nil {
+				m.selected = nil
+			} else {
+				return m, nil
+			}
+
 		}
 
 	}
@@ -107,7 +136,41 @@ func (m inboxModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m inboxModel) View() string {
-	return appStyle.Render(m.mails.View())
+	var s string
+	if m.selected != nil {
+		s = selectedEmailView(m.mails.SelectedItem().(item))
+	} else {
+		s = m.mails.View()
+	}
+
+	return appStyle.Render(s) + "\n"
+}
+
+func selectedEmailView(item item) string {
+	return fmt.Sprintf(
+		"Selected Email:\nTitle: %s\nDescription: %s\n",
+		item.Title(),
+		item.Description(),
+	)
+
+}
+
+func updateEmailView(msg tea.Msg, m inboxModel, email emailModel) (tea.Model, tea.Cmd) {
+	fmt.Sprintf("Subject: %s\nBody: %s\nSender: %s\nReceiver: %s\n",
+		email.subject, email.body, email.sender, email.receiver)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+		if msg.String() == "h" {
+			m.selected = nil
+			return m, nil
+		}
+	}
+	return m, nil
 }
 
 func main() {
